cf42151e07735d19d8c2741c6fa1aa09d9b483f5
Bernhard Beschow <shentey@gmail.com>
2026-01-21 21:04:32 +0100

    wip

--- mx8m/esdhc.c
+++ mx8m/esdhc.c
@@ -12,10 +12,13 @@
 #include <esdhc.h>
 #include <io.h>
 
+#define SDHC_BLKSIZE                   0x04
+
 #define SDHC_ARGUMENT                  0x08
 
 #define SDHC_TRNMOD                    0x0C
 #define SDHC_CMD_RESPONSE              (3 << 16)
+#define SDHC_CMD_DATA_PRESENT          BIT(21)
 
 #define SDHC_RSPREG0                   0x10
 /* ROC Response Register 1 0x0 */
@@ -25,7 +28,14 @@
 /* ROC Response Register 3 0x0 */
 #define SDHC_RSPREG3                   0x1C
 
+#define SDHC_HOSTCTL                   0x28
+
 #define SDHC_NORINTSTS                 0x30
+
+#define ESDHC_MIX_CTRL                 0x48
+#define ESDHC_MIX_CTRL_DMA_EN          BIT(0)
+
+#define SDHC_ADMASYSADDR               0x58
 
 struct imx_esdhc_config {
     uintptr_t base;
@@ -87,6 +97,13 @@ int esdhc_init(const struct sdhci_config *esdhc_cfg)
     return 0;
 }
 
+struct desc {
+    uint32_t addr;
+    uint16_t size;
+    uint8_t reserved;
+    uint8_t attr;
+};
+
 void esdhc_read_block(uint8_t *buf, uint16_t size)
 {
     uintptr_t base = esdhc_dev_cfg.base;
@@ -94,4 +111,17 @@ void esdhc_read_block(uint8_t *buf, uint16_t size)
     /* CMD16 */
     writel(0x00000200, base + SDHC_ARGUMENT);
     writel(16 << 24, base + SDHC_TRNMOD);
+
+    /* CMD18 */
+    struct desc desc = {
+        .addr = (uint32_t)buf,
+        .size = size,
+        .attr = 0x23,
+    };
+    writeq(&desc, SDHC_ADMASYSADDR);
+    writel(0x00087200, base + SDHC_BLKSIZE);
+    writel(ESDHC_MIX_CTRL_DMA_EN, base + ESDHC_MIX_CTRL);
+    writel(0x00000012, base + SDHC_HOSTCTL);
+    writel(0x00008000, base + SDHC_ARGUMENT);
+    writel(18 << 24 | SDHC_CMD_DATA_PRESENT, base + SDHC_TRNMOD);
 }
